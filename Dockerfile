dbf6bf9ce93291efbafae917c47e4a5854ba21a7
Italo Nicola <italo.niccola@gmail.com>
2024-08-09 07:55:14 -0300

    Map .env file automatically with run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # syntax=docker/dockerfile:1
 FROM python:3.12-slim-bullseye
-WORKDIR /devgrid-weather-docker
+WORKDIR /weather-docker
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 COPY . .
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run --name mongodb -p 27017:27017 -d --network mongodb mongodb/mongodb-community-server:latest &
-docker container run -p 5000:5000 -d --network mongodb --name devgrid-container weather_server &
+docker container run -p 5000:5000 -d -v $(pwd)/.env:/weather-docker/.env --network mongodb --name weather-container weather_server &
 wait -n
 
